1875b9fc20295a91d7c1d458017e6ddbf79d0933
Andrey Veprikov <av@rees46.com>
2017-06-08 18:36:03 +0300

    v3.0.3
    Plugin path.

--- rees46/lib/updates/3.0.3/1496933944.php
+++ rees46/lib/updates/3.0.3/1496933944.php
@@ -3,8 +3,12 @@
 // Удаляем файлы отсутствующие в текущей версии плагина
 // Версия плагина 3.0.3
 
-$remove_files_list = array('../actions/shopFrontendRees46.controller.php');
+$app_config = wa()->getConfig()->getAppConfig('shop');
+$plugin_path = $app_config->getPluginPath('rees46');
+$remove_files_list = array('/lib/actions/shopFrontendRees46.controller.php');
+
 foreach($remove_files_list as $file) {
+    $file = $plugin_path.$file;
     try {
         if (!file_exists($file) || !is_writable($file)) {
             throw new Exception('file not exists or not writable');
